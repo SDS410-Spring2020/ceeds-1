4cdc03ca43224bf08e355fe214e62b8505c899c2
Mirella213 <mhernandez@smith.edu>
2019-02-26 13:01:20 -0500

    update

---
+++ R/macleish.R
@@ -0,0 +1,35 @@
+#' Read in the current Whately data
+#' Read in the current Orchard data
+#' @importFrom magrittr %>%
+#' @export
+#' @examples 
+#' \dontrun{
+#' whately <- read_whately()
+#' nrow(whately)
+#' head(whately)
+#' }
+#' #' \dontrun{
+#' orchard <- read_orchard()
+#' nrow(orchard)
+#' head(orchard)
+#' }
+
+read_whately <- function() {
+  macleish <- etl::etl("macleish")
+  macleish %>%
+    etl::etl_update()
+  whately <- macleish %>%
+    dplyr::tbl("whately") %>%
+    dplyr::collect()
+  return(whately)
+}
+
+read_orchard <- function() {
+  macleish <- etl::etl("macleish")
+  macleish %>%
+    etl::etl_update()
+  orchard <- macleish %>%
+    dplyr::tbl("orchard") %>%
+    dplyr::collect()
+  return(orchard)
+}
