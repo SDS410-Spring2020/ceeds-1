3295203e46d4446e1b6e40d65c4c0f225ea23e84
Julia Lee <julialee64@Julias-MacBook-Air-2.local>
2019-04-30 15:07:32 -0400

    putting the shiny web docs in repo. MacLeishWeather

---
+++ MacLeishWeather/server.R
@@ -0,0 +1,160 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(RMySQL)
+library(etl)
+library(leaflet)
+library(RSQLite)
+library(ceeds)
+library(macleish)
+library(lubridate)
+library(shinydashboard)
+library(shiny)
+library(highcharter)
+library(timetk)
+library(kableExtra)
+library(plotly)
+# Define server logic required to draw a histogram
+shinyServer(function(input, output,session) {
+   
+
+  mac_data <- ceeds::read_whately()
+  whately <- purrr::pluck(mac_data, "whately")
+  orchard <- purrr::pluck(mac_data, "orchard")
+  WhatelyMet <- ceeds::get_daily(whately)
+  WhatelyMet <- ceeds::get_daily(whately)
+  daily_whately <- WhatelyMet
+  OrchardMet <- ceeds::get_daily(orchard)
+  daily_orchard <- OrchardMet
+  daily_orchard2 <- daily_orchard
+  daily_whately2 <- daily_whately
+  data_sets <- c("daily_orchard", "daily_whately")
+  # defining outputs
+  # output$name <- name = highchartOutput("name")
+  output$cw <- renderTable({
+    current;
+  })
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "WhatelyMet" = daily_whately,
+           "OrchardMet" = daily_orchard)
+  })
+  output$hc1 <- renderHighchart({
+    highchart(type = "stock") %>%
+      hc_add_series(datasetInput(),type = "line", hcaes(y = avgTemp, x = the_date), 
+                    name = "Avg temperature") %>%
+      hc_add_series(datasetInput(),type = "line", hcaes(y = mintemp, x = the_date), 
+                    name = "min temperature") %>%
+      hc_add_series(datasetInput(),type = "line", hcaes(y = maxtemp, x = the_date), 
+                    name = "max temperature") %>%
+      hc_exporting(enabled = TRUE) # enable exporting option 
+  })
+  output$hc2 <- renderHighchart({highchart(type = "stock") %>%
+      hc_add_series(datasetInput(),type = "bar", hcaes(y = precipitation, 
+                                                       x = the_date), name = "precipitation") %>%
+      hc_exporting(enabled = TRUE)})
+  
+  output$hc3 <- renderHighchart({highchart(type = "stock") %>%
+      hc_add_series(datasetInput(),type = "line", hcaes(y = maxwind, x = the_date)
+                    , name = "Max Wind") %>%
+      hc_exporting(enabled = TRUE)})
+  
+  # ggplot2
+  output$hc4 <- renderPlot({
+    x<-input$bins
+    bins <- seq(min(datasetInput()$avgWindSpeed), max(datasetInput()$avgWindSpeed), length.out = input$bins)
+    dirbins <- seq(min(datasetInput()$dir), max(datasetInput()$dir), length.out = input$bins)
+    ggplot(data = datasetInput(), aes(x = cut(dir,8) , fill = cut(maxwind,x))) +
+      geom_bar() + 
+      scale_x_discrete(name="Wind Direction",drop = FALSE, labels =c("N","NE","E","SE","S","SW","W", "NW")) +
+      coord_polar(start = -((30/1.5)/360) *(2*pi)) +
+      scale_colour_brewer(name = "Wind Speed (m/s)") 
+  })
+  
+  output$plot1 <- renderPlotly({
+    ggplotly(ggplot(data = datasetInput(), aes_string(x =input$xcol, y = input$ycol)) +
+               geom_point(color = "green", alpha = 0.3)) 
+  })
+  output$fast_stats <- renderTable({
+    x<-as.character(input$v)
+    data <-  datasetInput() %>%
+      filter(the_date >= ymd(as.character(input$start1)), 
+             the_date <=ymd(as.character(input$end1)))
+    data2 <-  data[, input$v, drop = FALSE] 
+    stats<-skimr::skim(data2) 
+    stats
+    
+  })
+  
+  
+  # download raw data 
+  output$choose_dataset <- renderUI({
+    selectInput("dataset2", "Data set", as.list(data_sets))
+  })
+  datasetInput2 <- reactive({
+    switch(input$dataset2,
+           "WhatelyMet" = daily_whately2,
+           "OrchardMet" = daily_orchard2)
+  })
+  output$choose_columns <- renderUI({
+    # If missing input, return to avoid error later in function
+    if (is.null(input$dataset2))
+      return()
+    
+    # Get the data set with the appropriate name
+    data1 <-  datasetInput2()
+    colnames <- names(data1)
+    
+    # Create the checkboxes and select them all by default
+    checkboxGroupInput("columns", "Choose columns", 
+                       choices  = colnames,
+                       selected = colnames)
+  })
+  output$data_table <- DT::renderDataTable({
+    if(is.null(input$dataset))
+      return()
+    if (is.null(input$columns) || !(input$columns %in% names(datasetInput2())))
+      return()
+    
+    # Keep the selected columns
+    data1 <-  datasetInput2()[, input$columns, drop = FALSE]
+    data1<-data1 %>%
+      filter(the_date >= ymd(as.character(input$start)), 
+             the_date <=ymd(as.character(input$end)))
+    
+    data1
+  })
+  
+  output$downloadData <- downloadHandler(
+    filename = function() { 
+      paste('Weather', '.csv', sep='') },
+    content = function(file) {
+      write.csv( datasetInput2()[, input$columns, drop = FALSE]%>%
+                   filter(the_date >= ymd(as.character(input$start)), 
+                          the_date <=ymd(as.character(input$end)))
+                 , file)
+    })
+  
+  # we want to switch tabs so we need to refresh session
+  # observeEvent allows reactive input and allows us to switch tabs
+  observeEvent(input$switchtab, {
+    newtab <- switch(input$tabs, 
+                     "About" = "Raw data", "Raw data"="About","About"="Current Weather", "Current Weather"="About", "About"="Historic Data", "Historic Data"="About",
+                     "Raw data"="Current Weather","Current Weather"="Raw data","Raw data"="Historic Data",
+                     "Historic Data"="Raw data","Historic Data"="Current Weather",
+                     "Current Weather"="Historic Data")
+    #update tab items 
+    updateTabItems(session, "tabs", newtab)
+  })
+ 
+  
+})
+
---
+++ MacLeishWeather/ui.R
@@ -0,0 +1,258 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+library(tidyverse)
+library(RMySQL)
+library(etl)
+library(leaflet)
+library(RSQLite)
+library(ceeds)
+library(macleish)
+library(lubridate)
+library(shinydashboard)
+library(shiny)
+library(highcharter)
+library(kableExtra)
+library(plotly)
+#install.packages("skimr")
+library(DT)
+library(skimr)
+library(shinydashboardPlus)
+library(base64enc)
+library(shiny)
+mac_data <- read_whately()
+whately <- purrr::pluck(mac_data, "whately")
+orchard <- purrr::pluck(mac_data, "orchard")
+daily_whately <- get_daily(whately)
+daily_orchard <- get_daily(orchard)
+last_whately <- get_lastyear(daily_whately)
+#getting current weather data
+current_w <- tail(whately, 1)
+current_o <- tail(orchard,1)
+# print(current[1,2])
+b64 <- base64enc::dataURI(file= "height_comparison.png", mime="image/png")
+b65 <- base64enc::dataURI(file= "paul.png", mime="image/png")
+b66 <- base64enc::dataURI(file= "MacLeish.png", mime="image/png")
+
+# Define UI for application that draws a histogram
+shinyUI(dashboardPage(skin = "black",
+                      # making a heading for the dashboard
+                      dashboardHeader(title = "MacLeish Weather", 
+                                      dropdownMenu()),
+                      #making a sidebar with tabs
+                      dashboardSidebar(
+                        # making a sidebar menu
+                        sidebarMenu(
+                          #making an id 
+                          id = "tabs",
+                          #creating menu items
+                          menuItem("Current Weather", 
+                                   tabName = "current", 
+                                   icon = icon("seedling")),
+                          menuItem("About", tabName = "about", 
+                                   icon = icon("newspaper")),
+                          menuItem("Historic Data", 
+                                   tabName = "historic", 
+                                   icon = icon("bar-chart-o")),
+                          menuItem("Download Data", 
+                                   tabName = "raw", 
+                                   icon = icon("table"))      
+                        )),
+                      # Drawing the dashboard body
+                      dashboardBody(
+                        # tab items 
+                        tabItems(
+                          tabItem(tabName = "current", 
+                                  fluidPage(
+                                    tags$head(
+                                      tags$style(HTML('
+                                                      .main-header .logo {
+                                                      font-family: "Geneva";
+                                                      font-size: 20px;
+                                                      }
+                                                      
+                                                      '))
+                                      ),
+                                    gradientBox(
+                                      title = span("OrchardMet Station", style = "font-family: Geneva"),
+                                      icon = "fa fa-apple-alt",
+                                      gradientColor = "teal", 
+                                      boxToolSize = "sm", 
+                                      footer = 
+                                        " ",
+                                      h5("as of:", current_o$when, align = "center",
+                                         style = "font-family: Geneva"),
+                                      p(current_o$temperature, "°C", 
+                                        style = "font-family: Geneva; font-size: 3em"),
+                                      h4("humidity  ", icon("tint"), br(),
+                                         current_o$rel_humidity, "%", align = "right",
+                                         style = "font-family: Geneva"), 
+                                      hr(style = "width: 40%; margin-top: 10px; margin-bottom: 10px", 
+                                         align = "right"),
+                                      h4("pressure  ", icon("plane-departure"), br(),
+                                         current_o$pressure, align = "right",
+                                         style = "font-family: Geneva"), 
+                                      hr(style = "width: 40%; margin-top: 10px; margin-bottom: 10px", 
+                                         align = "right"),
+                                      h4("precipitation ", icon("umbrella"), br(),
+                                         current_o$rainfall, "mm", align = "right",
+                                         style = "font-family: Geneva"), 
+                                      hr(style = "width: 40%; margin-top: 10px; margin-bottom: 10px", 
+                                         align = "right"),
+                                      h4("wind", icon("cloud"), br(),
+                                         current_o$wind_speed, "m/s", align = "right",
+                                         style = "font-family: Geneva"),
+                                      collapsible = FALSE
+                                    ),
+                                    gradientBox(
+                                      title = span("WhatelyMet Station", style = "font-family: Geneva"),
+                                      icon = "fa fa-leaf",
+                                      gradientColor = "teal", 
+                                      boxToolSize = "sm",
+                                      footer =
+                                        " ",  
+                                      h5("as of:", current_w$when, align = "center",
+                                         style = "font-family: Geneva"),
+                                      p(current_w$temperature, "°C",
+                                        style = "font-family: Geneva; font-size: 3em"),
+                                      h4("humidity  ", icon("tint"), br(),
+                                         current_w$rel_humidity, "%", align = "right",
+                                         style = "font-family: Geneva"), 
+                                      hr(style = "width:40%; margin-top: 10px; margin-bottom: 10px", 
+                                         align = "right"),
+                                      h4("pressure  ", icon("plane-departure"), 
+                                         br(),
+                                         current_w$pressure, align = "right",
+                                         style = "font-family: Geneva"), 
+                                      hr(style = "width:40%; margin-top: 10px; margin-bottom: 10px", 
+                                         align = "right"),
+                                      h4("precipitation ", icon("umbrella"), br(),
+                                         current_w$rainfall, "mm", align = "right",
+                                         style = "font-family: Geneva"), 
+                                      hr(style = "width:40%; margin-top: 10px; margin-bottom: 10px", 
+                                         align = "right"),
+                                      h4("wind", icon("cloud"), br(),
+                                         current_w$wind_speed, "m/s", align = "right",
+                                         style = "font-family: Geneva"),
+                                      collapsible = FALSE
+                                    )
+                                      )),
+                          tabItem(tabName = "about", 
+                                  h1("Welcome to the MacLeish weather app!", align = "center", 
+                                     style = "font-family: Geneva"),
+                                  hr(),
+                                  p("Here you will find current weather, historical data (from 
+                                    January 2012 to present) and data you can download of the two 
+                                    weather stations at the MacLeish Field Station of Smith 
+                                    College.", style = "font-family: 'Geneva'; 
+                                    margin-left: 28%; margin-right: 28%"),
+                                  img(src=b66, height = 244, width = 279, 
+                                      style="display: block; margin-left: auto; margin-right: auto;" ),
+                                  br(),
+                                  p("The MacLeish Field Station weather collection sites are 
+                                    accessed at the end of Poplar Hill Road in Whately, 
+                                    Massachusetts, USA (Lat: 42 deg. 26 min. 56 sec. N Long: 72 deg.
+                                    40 min. 50 sec. W).", style = "font-family: 'Geneva';
+                                    margin-left: 28%; margin-right: 28%"),
+                                  p("Weather measurements are made at two locations at the MacLeish
+                                    field station. On the WhatelyMet tower, meteorological 
+                                    instruments (except the rain gauge) are mounted at the top of a 
+                                    tower 25.3 m tall, well above the surrounding forest canopy. 
+                                    Ground elevation is 250.8 m above sea level. OrchardMet is a 
+                                    standard, 10 m meteorological station, elevation 258 m above 
+                                    sea level. Temperature, relative humidity, and radiation are 
+                                    measured 2 m above the ground. The tower is located in a forest
+                                    clearing.", style = "font-family: 'Geneva'; margin-left: 28%;
+                                    margin-right: 28%"),
+                                  br(),
+                                  img(src=b64, height = 264, width = 345.4, 
+                                      style="display: block; margin-left: auto; margin-right: auto;"),
+                                  h4("height comparison between weather station towers, trees and 
+                                     people", align = "center", style = "font-family: 'Geneva; 
+                                     font-style: italic; font-size: 14px"),
+                                  p("We strive to make this data more accessible to those who can 
+                                    use it, from Smith College Statistical & Data Sciences students
+                                    who need large datasets to practice data visualization & data 
+                                    wrangling methods, to local researchers in the Pioneer Valley.",
+                                    style = "font-family: 'Geneva'; margin-left: 28%; 
+                                    margin-right: 28%"),
+                                  br(),
+                                  img(src=b65, height = 249, width = 322, 
+                                      style="display: block; margin-left: auto; margin-right: auto"),
+                                  h4("meet the weather station manager, Paul Wetzel of CEEDS ", 
+                                     align = "center", style = "font-family: 'Geneva; 
+                                     font-style: italic; font-size: 14px"),
+                                  p("All the data visualizations you see are interactive so feel 
+                                    free to click around and learn about your local weather!", 
+                                    style = "font-family: 'Geneva'; margin-left: 28%;
+                                    margin-right: 28%"),
+                                  br(),
+                                  p("Sincerely,", style = "font-family: 'Geneva'"),
+                                  p(a("Marta, Mirella, and Julia (SDS 410, spring 2019)",
+                                      href = "https://github.com/beanumber/ceeds"), 
+                                    style = "font-family: 'Geneva'"),
+                                  p(a("Paul R. Wetzel", href = "mailto: pwetzel@smith.edu"),
+                                    style = "font-family: 'Geneva'"
+                                  )
+                                  ),
+                          tabItem(tabName = "historic", h2("Historic Data", align = "center"),
+                                  fluidPage(splitLayout(cellWidths = c("15%","16%","16%","16%","16%","16%")),
+                                            box(selectInput("dataset", "Dataset", c("WhatelyMet", "OrchardMet")), width = 12),
+                                            box(highchartOutput("hc2"), width = 12, collapsible = T, solidHeader = T, 
+                                                title = "Precipitation",collapsed =T),
+                                            box(highchartOutput("hc3"),width = 12,collapsible = T,solidHeader = T, 
+                                                title = "MAX Wind Speed", collapsed =T),
+                                            box(highchartOutput("hc1"),width = 12, collapsible = T,solidHeader = T, 
+                                                title = "Temperature", collapsed =T),
+                                            box(plotOutput("hc4"), width = 12,collapsible = T,solidHeader = T, 
+                                                title = "Wind Direction", collapsed =T,
+                                                sliderInput("bins",
+                                                            "Number of bins:",
+                                                            min = 4,
+                                                            max = 15,
+                                                            value = 7)
+                                            ),
+                                            box(plotlyOutput("plot1"), width = 12, collapsible = T,solidHeader = T, 
+                                                collapsed =T,title = "Other Variables (correlation)", sidebarPanel(
+                                                  selectInput('xcol', 'X Variable', names(ceeds::get_daily(whately_2015))),
+                                                  selectInput('ycol', 'Y Variable', names(ceeds::get_daily(whately_2015))),
+                                                  selected=names(ceeds::get_daily(whately_2015))[[2]])),
+                                            box(tableOutput("fast_stats"),width = 12,collapsible = T,solidHeader = T, 
+                                                title = "Summary Statistics Table", collapsed =T, sidebarPanel(
+                                                  selectInput('v', 'Variable',names(ceeds::get_daily(whately_2015))),
+                                                  dateInput("start1",
+                                                            "Start date:",
+                                                            value = 2012-01-01,
+                                                            format = "yyyy-mm-dd"),
+                                                  dateInput("end1",
+                                                            "End date:",
+                                                            value = now(),
+                                                            format = "yyyy-mm-dd"))))),
+                          tabItem(tabName = "raw", h2("Raw Data", align = "center"),
+                                  fluidPage(splitLayout(cellWidths = c("45%","55%"),
+                                                        box(sidebarPanel(
+                                                          selectInput("dataset2", "Dataset", c("WhatelyMet", "OrchardMet")),
+                                                          uiOutput("choose_columns"),
+                                                          br(),
+                                                          a(href = "https://gist.github.com/4211337", "Source code"),
+                                                          downloadButton('downloadData', 'Download'),
+                                                          width=12, dateInput("start",
+                                                                              "Start date:",
+                                                                              value = 2012-01-01,
+                                                                              format = "yyyy-mm-dd"),
+                                                          dateInput("end",
+                                                                    "End date:",
+                                                                    value = now(),
+                                                                    format = "yyyy-mm-dd")
+                                                        ),width=12),box(DT::dataTableOutput("data_table"), width = 24,collapsible = T,
+                                                                        solidHeader = T, title="Data table")))) 
+                          
+                                  )#end tabItems
+                                  )#end body
+                      )
+)
